040ce689b057fbea712be341a9aaa401a42cfaa7
Marcin00Czarnecki <marcin00czarnecki@gmail.com>
2021-07-09 09:47:06 +0200

    BookApp

---
+++ myapi/serializers.py
@@ -0,0 +1,20 @@
+from books.models import Book, Author, Language
+from rest_framework import serializers
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
